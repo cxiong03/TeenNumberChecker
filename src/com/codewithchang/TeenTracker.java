3933384582adbb4d4c63b4de924ecbb275c119e6
Chang <cnxiong03@gmail.com>
2021-02-19 20:29:48 -0500

    refactor code

--- src/com/codewithchang/TeenTracker.java
+++ src/com/codewithchang/TeenTracker.java
@@ -3,8 +3,10 @@ package com.codewithchang;
 public class TeenTracker {
 
     public static boolean hasTeen(int age1, int age2, int age3) {
-        if (age1 >= 13 && age1 <= 19 || age2 >= 13 && age2 <= 19 || age3 >= 13 && age3 <= 19) {
+//        if (age1 >= 13 && age1 <= 19 || age2 >= 13 && age2 <= 19 || age3 >= 13 && age3 <= 19) {
 //            System.out.println("True");
+
+        if(isTeen(age1) || isTeen(age2) || isTeen(age3)) {
             return true;
         }
 //        System.out.println("False");
@@ -12,7 +14,7 @@ public class TeenTracker {
     }
 
     public static boolean isTeen(int age) {
-        if (age > 13 && age < 19) {
+        if (age >= 13 && age <= 19) {
             return true;
         }
         return false;
